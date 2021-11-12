0576b0c52a70114e0459f43afcb3f7070b818ad9
Rami Assaf <ramiw.assaf@gmail.com>
2021-11-12 18:12:58 +0000

    minor change on seed file to add destroy_all part 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
 require 'json'
 require 'open-uri'
+
+puts "Cleansing movies..."
 Movie.destroy_all
+puts "Creating movies..."
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 movie_serialised = URI.open(url).read
 movie = JSON.parse(movie_serialised)
 movie['results'].each do |film|
   Movie.create(title: film['title'], overview: film['overview'], poster_url: "https://image.tmdb.org/t/p/original#{film['poster_path']}", rating: film['vote_average'])
 end
+puts "Movies created!"
 
 # Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 # Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
